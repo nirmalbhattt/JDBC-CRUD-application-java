bc670efbd69ab77148baf83ecebec690efddd6dc
Nirmal Bhatt <nb9384116@gmail.com>
2025-10-03 08:15:38 +0530

    Create employee_db.sql

---
+++ employee_db.sql
@@ -0,0 +1,11 @@
+
+CREATE DATABASE employee_db;
+
+USE employee_db;
+
+CREATE TABLE IF NOT EXISTS employees (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(100) NOT NULL,
+  salary DOUBLE NOT NULL,
+  department VARCHAR(100) NOT NULL
+);
